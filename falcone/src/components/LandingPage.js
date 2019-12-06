8589b36e33e85df4ea4ff2bed82147b5c576add5
shubham860 <shubham.chauhan860@gmail.com>
2019-12-07 00:11:02 +0530

    commit-1

---
+++ falcone/src/components/LandingPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div className="mainContainer">
+           <div className="header">
+
+           </div>
+           <div className="mainPanel">
+               <div className="staticPanel"></div>
+               <div className="dynamicPanel"></div>
+           </div>
+           <div className="footer">
+
+           </div>
+        </div>
+    );
+};
+
+export default LandingPage;
