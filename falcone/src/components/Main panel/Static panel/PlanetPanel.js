c05fe2fd138b0b43e68774a280a94cef52caaed2
shubham860 <shubham.chauhan860@gmail.com>
2019-12-26 01:21:25 +0530

    commit-20

--- falcone/src/components/Main panel/Static panel/PlanetPanel.js
+++ falcone/src/components/Main panel/Static panel/PlanetPanel.js
@@ -1,27 +1,42 @@
 import React, {Component} from 'react';
-import './PlanetPanel.scss'
+import './PlanetPanel.scss';
+import axios from 'axios';
+import planet1 from '../../../assets/images/5.png';
+import planet2 from '../../../assets/images/6.png';
+import planet3 from '../../../assets/images/7.png';
+import planet4 from '../../../assets/images/8.png';
+import planet5 from '../../../assets/images/9.png';
+import planet6 from '../../../assets/images/10.png';
 
 class PlanetPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            planet1 : false,
-            planet2 : false,
-            planet3 : false,
-            planet4 : false,
-            planet5 : false,
-            planet6 : false
+            planets : [],
+            planetsImages : [{planet1},{planet2},{planet3},{planet4},{planet5},{planet6}]
         }
     }
 
-    handler = (event) => {
-       this.setState({
-           [event.target.name] : ![event.target.value]
-       })
-    };
+    componentDidMount() {
+        axios
+            .get(`https://findfalcone.herokuapp.com/planets`)
+            .then(response => {
+                this.setState({
+                    planets: response.data
+                })
+            })
+            .catch(error => {
+                alert(error)
+            });
+
+
+    }
+
+
+
 
     render() {
-        console.log(this.state);
+        console.log(this.state.planets);
         return (
             /* Planet panel :- it contains all the planets images and Names */
             <div className="planetPanel">
